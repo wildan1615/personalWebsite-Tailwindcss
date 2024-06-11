eb5a952eb7bc4f6ec6df9fe442182a3968276247
Wildan_A <wildanaminudin16@gmail.com>
2024-06-11 10:01:28 +0700

    membuat halaman home dan about

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0ea5e9",
+        secondary: "#94a3b8",
+        dark: "#020617",
+      },
+    },
+  },
+  plugins: [],
+};
